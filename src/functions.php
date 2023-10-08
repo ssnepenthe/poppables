2aa3c337efdb484d1f00a732d4d428043c4a76fb
ssnepenthe <ssnepenthe@users.noreply.github.com>
2023-10-07 19:47:26 -0700

    Forgot to add an alias helper func

--- src/functions.php
+++ src/functions.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace Poppables;
 
+if (! function_exists(__NAMESPACE__ . '\\alias')) {
+    function alias(string $originalId)
+    {
+        return new Alias($originalId);
+    }
+}
+
 if (! function_exists(__NAMESPACE__ . '\\extend')) {
     function extend($invokable)
     {

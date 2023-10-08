201e9e483470b841d98a601dac0a00879e64684f
ssnepenthe <ssnepenthe@users.noreply.github.com>
2023-10-07 19:47:01 -0700

    Add alias feature

---
+++ src/Alias.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Poppables;
+
+use Pimple\Container;
+
+final class Alias implements Poppable
+{
+    private string $originalId;
+
+    public function __construct(string $originalId)
+    {
+        $this->originalId = $originalId;
+    }
+
+    public function pop(string $id, Container $pimple)
+    {
+        $pimple[$id] = $pimple->factory(fn (Container $pimple) => $pimple[$this->originalId]);
+    }
+}
---
+++ tests/AliasTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Poppables\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Poppables\Container;
+
+use function Poppables\alias;
+
+class AliasTest extends TestCase
+{
+    public function testAlias()
+    {
+        $container = new Container();
+
+        $container->set('key', 'value');
+        $container->set('alias-key', alias('key'));
+
+        $this->assertTrue($container->has('alias-key'));
+        $this->assertSame('value', $container->get('alias-key'));
+    }
+
+    public function testAliasIsStoredAsFactory()
+    {
+        $container = new Container();
+
+        $container->set('key', 0);
+        $container->set('alias-key', alias('key'));
+
+        $this->assertSame(0, $container->get('key'));
+        $this->assertSame(0, $container->get('alias-key'));
+
+        $container->set('key', 1);
+
+        $this->assertSame(1, $container->get('key'));
+        $this->assertSame(1, $container->get('alias-key'));
+    }
+}
